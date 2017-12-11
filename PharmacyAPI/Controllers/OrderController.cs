05c8870ca08e883d1b82d9db4dd943e56742c7a4
Niyaz <Mail>
2017-12-11 08:22:22 +0300

    Добавьте файлы проекта.

---
+++ PharmacyAPI/Controllers/OrderController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PharmacyAPI.Controllers
+{
+    public class OrderController : ApiController
+    {
+        Pharmacy db = new Pharmacy();
+        // GET api/<controller>
+        public IEnumerable<Order> Get(string id)
+        {
+            return db.Orders.Where(c => c.Login == id).ToList();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public IHttpActionResult Post([FromBody]Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return Ok(order);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
